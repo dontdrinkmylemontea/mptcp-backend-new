084b2615b96d0af861956120b97762f12fedf361
lxy <492861128@qq.com>
2019-04-29 09:07:07 +0800

    代码copy到服务端

--- src/server/server.js
+++ src/server/server.js
@@ -5,14 +5,33 @@ const port = 8081;
 
 app.use((req, res, next) => {
   res.set("Content-Type", "text/json");
-  res.setDefaultEncoding("utf-8");
   res.append("Access-Control-Allow-Origin", ["*"]);
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.append("Access-Control-Allow-Headers", "Content-Type");
+  console.log(`request for ${req.path} received`);
   next();
 });
 
-app.get("/ping", (req, res) => {});
+app.use(bodyParser.json());
+
+const pingStatus = {
+  hasError: false,
+  state: []
+};
+
+//需要轮询
+app.get("/ping", (req, res) => {
+  if (req.query.sub == 0) {
+    res.status(200).send(getObj(0, pingStatus.state));
+    pingStatus.hasError = false;
+    pingStatus.state = [];
+    ping(pingStatus);
+  } else if (pingStatus.hasError) {
+    res.status(500).send(getObj(-1, pingStatus.state));
+  } else {
+    res.status(200).send(getObj(0, pingStatus.state));
+  }
+});
 
 app.post("/config", (req, res) => {});
 
