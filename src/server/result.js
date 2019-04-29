d975b1a8ee01b27565522004a4b021eed4d36ff4
lxy <492861128@qq.com>
2019-04-29 17:01:36 +0800

    读取文件

--- src/server/result.js
+++ src/server/result.js
@@ -1,8 +1,22 @@
 const fs = require("fs");
 
-const filePath = "/home/yiyi/gd/backend/script/test.rb";
+const filePath = "/home/yiyi/gd/backend/result/topo1-buffersize-blest-1004.txt";
 
 exports.result = function(req, res) {
+  const buf = new Buffer.alloc(1024);
+  const begin = 0;
+  fs.open(filePath, "r+", function(err, fd) {
+    if (err) throw err;
+    let readed = 0;
+    while (readed > 0) {
+      fs.read(fd, buf, begin, 1024, 0, function(error, bytes) {
+        if (error) throw error;
+        res.send(buf.toString());
+        readed = bytes;
+      });
+    }
+  });
+
   fs.readFile(filePath, function(err, data) {
     if (err) throw err;
     res.send(data);
